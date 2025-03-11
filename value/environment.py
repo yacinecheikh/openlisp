9d0132fc6f7302b2f1046df4ac0608fd78af0145
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2025-03-11 16:46:33 +0100

    add wrappers for dict key definitions

--- value/environment.py
+++ value/environment.py
@@ -1,4 +1,6 @@
-from value import hashtable, unique
+from value import hashtable, unique, string
+from value.types import hashtable_type, symbol_type
+from value.value import Value
 
 
 def environment(parent=unique.nil):
@@ -8,6 +10,13 @@ def environment(parent=unique.nil):
     lisp_value = hashtable.hashtable(bindings)
     bindings["current-scope"] = lisp_value
     return lisp_value
+
+# wrapper over hashtable.setkey
+# uses symbols instead of strings as keys
+def bind(env: Value, symb: Value, value: Value):
+    assert env.type == hashtable_type
+    assert symb.type == symbol_type
+    hashtable.setkey(env, string.string(symb.value), value)
 
 
 global_environment = environment()
--- value/hashtable.py
+++ value/hashtable.py
@@ -1,6 +1,12 @@
-from .types import hashtable_type
+from .types import hashtable_type, str_type
 from .value import Value
 
 def hashtable(dictionary):
     return Value(type=hashtable_type, value=dictionary)
+
+def setkey(table: Value, string: Value, value: Value):
+    assert table.type == hashtable_type
+    assert string.type == str_type
+
+    table.value[string.value] = value
 
